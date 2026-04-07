ba0aeb005c1fff3df74115fdc353ff0f94381cfa
vivekkumar <tvivekkumar@gmail.com>
2026-04-07 10:23:20 +0530

    version4

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,7 @@
 # write a code  to check give word is palindrome or not
+# getting input from the user
 word = input('enter your input: ')
+# check the condition whether user input word is palindrome or not
 if word == word[::-1]:
 	print ("The word", word, "is palindrome")
 else:
