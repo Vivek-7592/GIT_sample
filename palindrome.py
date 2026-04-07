614945bf0f047a4ef90a3bf58220dbdc24896e09
vivekkumar <tvivekkumar@gmail.com>
2026-04-07 10:15:37 +0530

    version2

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,6 @@
 #write a code  to check give word is palindrome or not
-word = 'mom'
-is_palindrome=word==word[::-1]
-print("is palindrome")
-
+word = input('enter your input: ')
+if word == word[::-1]:
+	print ("The word", word, "is palindrome")
+else:
+	print ("The word", word, "is not a palindrome")
